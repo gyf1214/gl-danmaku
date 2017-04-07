d366930ee34a53310cf108b6edede2d54da77217
gyf1214 <gyf1214@gmail.com>
2017-04-07 16:49:04 +0800

    WIP

--- src/scene/scene1.cc
+++ src/scene/scene1.cc
@@ -118,9 +118,9 @@ public:
     Light light() {
         return Light(
             glm::vec4(0.0f, 0.0f, 0.0f, 1.0f),
-            glm::vec3(1.0f, 0.6f, 0.2f),
+            glm::vec3(1.0f, 0.4f, 0.1f),
             glm::vec3(0.0f, 0.0f, 0.0f),
-            glm::vec4(0.3f, 0.0f, 0.3f / 10.0f / 10.0f, 1.0f)
+            glm::vec4(1.0f, 0.0f, 0.0f, 1.0f)
         );
     }
 };
