4ec7882aa293595122d74b69f45c76207d5f8ded
gyf1214 <gyf1214@gmail.com>
2017-05-30 16:21:22 +0800

    new reimu imported

--- include/log.hpp
+++ include/log.hpp
@@ -30,24 +30,40 @@ struct LogStream {
     }
 };
 
-#define LOG_TO(x, log, fail) LogStream(x, __FILE__, __LINE__, log, fail)
+#ifndef __FILENAME__
+#define __FILENAME__ __FILE__
+#endif
+
+#define LOG_TO(x, log, fail) LogStream(x, __FILENAME__, __LINE__, log, fail)
 
 #ifndef DEFAULT_LOG
 #define DEFAULT_LOG std::cerr
 #endif
 
+#define EMPTY       LOG_TO(DEFAULT_LOG, false, false)
 #define LOG         LOG_TO(DEFAULT_LOG, true, false)
-#define LOG_IF(x)   ({ bool __log = (x) ? true : false; LOG_TO(DEFAULT_LOG, __log, false); })
 #define FAIL        LOG_TO(DEFAULT_LOG, true, true)
 
+#ifndef NDEBUG
+#define LOG_IF(x)   ({ bool __log = (x) ? true : false; LOG_TO(DEFAULT_LOG, __log, false); })
+#else
+#define LOG_IF(x)   EMPTY
+#endif
+
+#ifndef NCHECK
 #define CHECK(x)    ({ bool __fail = (x) ? false : true; LOG_TO(DEFAULT_LOG, __fail, __fail); })
+#else
+#define CHECK(x)    EMPTY
+#endif
+
 #define CHECK_EQ(x, y) CHECK((x) == (y))
 #define CHECK_NQ(x, y) CHECK((x) != (y))
+#define CHECK_NORM(x)  CHECK_EQ(cmp(glm::length(x), 1.0f), 0)
 
 #ifndef NDEBUG
-#define CHECK_GL()  ({ GLenum err = glGetError(); CHECK_EQ(err, GL_NO_ERROR) << "gl error: " << err; })
+#define CHECK_GL      ({ GLenum err = glGetError(); CHECK_EQ(err, GL_NO_ERROR) << "gl error: " << err; })
 #else
-#define CHECK_GL()
+#define CHECK_GL
 #endif
 
 #endif
